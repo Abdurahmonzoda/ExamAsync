f2d6924063e7abd2be14a99a26887e634fc0c4f5
Abdurahmonzoda <86366309+Abdurahmonzoda@users.noreply.github.com>
2022-09-19 20:13:48 +0500

    project worked 100%

---
+++ Services/Services/QuotesServices.cs
@@ -0,0 +1,69 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Emtities;
+using Dapper;
+
+namespace Services.Services
+{
+    public class QuotesServices
+    {
+
+        private string _connectionString;
+        public QuotesServices()
+        {
+            _connectionString = "Server = 127.0.0.1; Port = 5433; Database = Quotes; User Id = postgres; Password = 45sD67ghone;";
+        }
+
+        public async Task<int> UpdateQuotes(Quotes quote)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(_connectionString))
+            {
+                string sql = $"UPDATE  quotes SET author = '{quote.Author}', quotesText = '{quote.QuotesText}' WHERE id = '{quote.Id}'";
+                var response = await connection.ExecuteAsync(sql);
+                return response;
+            }
+        }
+        public async Task<int> DeleteQuotes(int id)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(_connectionString))
+            {
+                string sql = $"DELETE FROM quotes WHERE id = '{id}'";
+                var response = await connection.ExecuteAsync(sql);
+                return response;
+            }
+        }
+        public async Task<List<Quotes>> GetQuotes()
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(_connectionString))
+            {
+                var sql = $"SELECT * FROM quotes";
+                var list = await connection.QueryAsync<Quotes>(sql);
+                return list.ToList();
+            }
+        }
+
+        public async Task<List<Quotes>> GetQuotesById(int id)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(_connectionString))
+            {
+                var sql = $"SELECT * FROM quotes JOIN category ON quotes.categoryid = category.id where quotes.categoryid = {id};";
+                var list = await connection.QueryAsync<Quotes>(sql);
+                return list.ToList();
+            }
+        }
+        public async Task<Quotes> GetQuotesRendom()
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(_connectionString))
+            {
+                var sql = $"select * from Quotes order by random() limit 1 ;";
+                var list = await connection.QuerySingleAsync<Quotes>(sql);
+                return list;
+            }
+        }
+
+    }
+}
---
+++ Web/Controllers/QuotesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Services;
+using Domain.Emtities;
+namespace Quote.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class QuotesController
+{
+
+    private QuotesServices _contactService;
+    public QuotesController()
+    {
+        _contactService = new QuotesServices();
+    }
+    [HttpPut]
+    public async Task<int> UpdateQuotes(Quotes quote)
+    {
+        return await _contactService.UpdateQuotes(quote);
+    }
+    [HttpDelete]
+    public async Task<int> DeleteQuotes(int id)
+    {
+        return await _contactService.DeleteQuotes(id);
+    }
+    [HttpGet]
+    public async Task<List<Quotes>> GetQuotes()
+    {
+        return await _contactService.GetQuotes();
+    }
+    [HttpGet("id")]
+    public async Task<List<Quotes>> GetQuotesById(int id)
+    {
+        return await _contactService.GetQuotesById(id);
+    }
+    [HttpGet("rendom")]
+    public async Task<Quotes> GetQuotesRendom()
+    {
+        return await _contactService.GetQuotesRendom();
+    }
+
+
+
+
+}
+
